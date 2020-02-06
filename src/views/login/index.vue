743f5bd54978236a81fe472b096b20780dc547bb
webhym <webhuangyanmeng@163.com>
2020-02-06 20:07:24 +0800

    表单元素的校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,11 +6,11 @@
     <el-card class="my-card">
       <img src="../../assets/logo_index.png" alt />
       <!-- 表单 -->
-      <el-form :model="loginForm">
-        <el-form-item>
+      <el-form :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="loginForm.code"
             placeholder="请输入验证码"
@@ -33,11 +33,42 @@
 export default {
   name: "app-login",
   data() {
+    //自定义手机号校验
+    const checkMobile = (rule, value, callback) => {
+      //进行校验(1开头，第二位3-9数字，其余剩下的9位数字)
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        callback(new Error("手机号输入错误,请重新输入"));
+      } else {
+        callback();
+      }
+    };
     return {
       //表单对应的数据对象
       loginForm: {
         mobile: "",
         code: ""
+      },
+      //表单对应校验规则
+      loginRules: {
+        //required:是否必选,
+        // massage:错误提示,
+        // trigger:触发校验时机
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          {
+            validator: checkMobile,
+            trigger: "blur"
+          }
+        ],
+        code: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          {
+            //len 输入内容必须是6位 min max 指定字符串的范围
+            len: 6,
+            message: "验证码6个字符",
+            trigger: "blur"
+          }
+        ]
       }
     };
   }
