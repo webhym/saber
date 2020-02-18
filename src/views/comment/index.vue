6070e30affc59f2ee4198991a59266e117168829
webhym <webhuangyanmeng@163.com>
2020-02-18 16:00:17 +0800

    评论管理-切换状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -14,8 +14,13 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="small" v-if="scope.row.comment_status" type="danger">关闭评论</el-button>
-            <el-button size="small" v-else type="success">打开评论</el-button>
+            <el-button
+              @click="toggleStatus(scope.row)"
+              size="small"
+              v-if="scope.row.comment_status"
+              type="danger"
+            >关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" size="small" v-else type="success">打开评论</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -51,6 +56,44 @@ export default {
     this.getComments();
   },
   methods: {
+    //切换状态
+    toggleStatus(row) {
+      //解释：row其实就是comments数组遍历后的每一个对象
+      //请求：put comments/status
+      //键值对参数:?article_id=文章ID
+      //请求体传参:allow_comment=想修改的状态
+      //关闭打开提示不一样:
+      //关闭您是否确定要关闭该文章的评论功能,关闭后用户无法对文章进行评论
+      //打开:您是否确定要打开该文章的评论功能
+      //row中有comment_status标识当前文章状态
+      const text1 =
+        "您是否确定要关闭改文章的评论功能,关闭后用户无法对文章进行评论";
+      const text2 = "您是否确定要打开改文章的评论功能";
+      this.$confirm(row.comment_status ? text1 : text2, "温馨提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(async () => {
+        try {
+          //res.data.data.allow_comment修改后的状态
+          //请求
+          const res = await this.$http.put(
+            `comments/status?article_id=${row.id}`,
+            {
+              allow_comment: !row.comment_status
+            }
+          );
+          //提示
+          this.$message.success(
+            res.data.data.allow_comment ? "打开成功" : "关闭成功"
+          );
+          // 修改当前行文章的评论状态
+          row.comment_status = res.data.data.allow_comment;
+        }catch(e){
+            this.$message.error('操作失败')
+        }
+      }).catch(() => {})
+    },
     // 分页
     pager(newPage) {
       this.reqParams.page = newPage;
